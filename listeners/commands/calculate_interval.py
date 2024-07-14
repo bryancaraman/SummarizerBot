4b8f26a094469cb0c149b6b3701f5e84a802a38c
bcaraman-godaddy <bcaraman@godaddy.com>
2024-07-13 22:20:57 -0500

    Calculated time intervals, retrieved conversation from channel

--- listeners/commands/calculate_interval.py
+++ listeners/commands/calculate_interval.py
@@ -1,19 +1,25 @@
+import datetime
 import time
-from .find_word import find_word
 
-def calculate_interval(input):
-    time = 0
-    if find_word('hour')(input):
-        # calc 1 hour
-        pass
-    elif find_word('day')(input):
-        # calc 1 day
-        pass
-    elif find_word('week')(input):
-        pass
-    elif find_word('days')(input):
-        pass
+def calculate_interval(time_input):
+    if 'week' in time_input:
+        beginning_of_today = datetime.date.today()
+        time_since_monday = datetime.timedelta(days=beginning_of_today.weekday())
+        return int(beginning_of_today.strftime("%s")) - time_since_monday.days * 86400
+    elif 'days' in time_input:
+        # Beginning of the day _ days back
+        split_input = time_input.split()
+        beginning_of_today = int(datetime.date.today().strftime("%s"))
+        days_ago = int(split_input[0]) * 86400
+        return beginning_of_today - days_ago
+    elif 'day' in time_input:
+        # Since the beginning of today
+        return datetime.date.today().strftime("%s")
+    elif 'hours' in time_input:
+        split_input = time_input.split()
+        return time.time() - int(split_input[0]) * 3600
+    elif 'hour' in time_input:
+        return time.time() - 3600
     else:
-        pass
-    return time
+        return 'Input validation failed'
         
--- listeners/commands/summarize_command.py
+++ listeners/commands/summarize_command.py
@@ -1,7 +1,12 @@
+import os
 from slack_bolt import Ack, Respond
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 from logging import Logger
 from .validate_input import validate_input
 from .calculate_interval import calculate_interval
+
+client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 def summarize_command_callback(command, ack: Ack, respond: Respond, logger: Logger):
     try:
@@ -15,10 +20,22 @@ def summarize_command_callback(command, ack: Ack, respond: Respond, logger: Logg
         
         respond(f"Summary of the last {command['text']}: ")
 
-        # {command['channel_id']} for id of channel
-
-        # Get messages within time interval
+        # Calculate when the time interval for getting messages
         oldest_time = calculate_interval(command['text'])
+        
+        channel_id = command['channel_id']
+        conversation_history = []
+
+        try:
+            result = client.conversations_history(channel=channel_id, inclusive=True, oldest=oldest_time)
+            conversation_history = result['messages']
+        except SlackApiError as e:
+            logger.error("Error getting conversation: {}".format(e))
+
+        # STRETCH GOAL: GET MESSAGES IN THREADS AS WELL
+
+        # Get array in order from oldest message to newest message
+        conversation_history.reverse()
         
     except Exception as e:
         logger.error(e)
